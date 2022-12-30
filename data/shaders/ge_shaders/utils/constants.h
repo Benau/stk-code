8eac9120979c18d1e63c515597b046c7bfe1a02d
Benau <Benau@users.noreply.github.com>
2022-12-30 23:54:37 +0800

    sc

---
+++ data/shaders/ge_shaders/utils/constants.h
@@ -0,0 +1,5 @@
+layout(constant_id = 0) const bool BIND_TEXTURES_AT_ONC = false;
+layout(constant_id = 1) const bool BIND_MESH_TEXTURES_AT_ONC = false;
+layout(constant_id = 2) const bool DIFFERENT_TEXTURE_PER_DRA = false;
+layout(constant_id = 3) const int SAMPLER_SIZ = 512;
+layout(constant_id = 4) const int TOTAL_MESH_TEXTURE_LAYE = 8;
